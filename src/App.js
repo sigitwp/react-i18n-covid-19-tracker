83cd48e5c792e7b48afeb3a567a78c68d095e250
sigit <sigit.setiaji@vizitrip.com>
2020-06-29 09:42:49 +0700

    Update i18n translation & implement in App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
+import { withTranslation } from 'react-i18next';
+
 import coronaImage from "./images/image.png";
 
 class App extends React.Component {
@@ -26,8 +28,21 @@ class App extends React.Component {
 
     render() {
         const { data, country } = this.state;
+        const { t, i18n } = this.props;
+        console.log("#i18n: ", i18n);
+        console.log("#i18n.language: ", i18n.language);
         return (
             <div className={styles.container}>
+
+                <h1 className="App-title">
+                    { this.props.t('welcome.title') }
+                </h1>
+
+                <div>
+                    <button onClick={() => i18n.changeLanguage('id')}>id</button>
+                    <button onClick={() => i18n.changeLanguage('en')}>en</button>
+                </div>
+
                 <img className={styles.image} src={coronaImage} alt="COVID-19" />
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
@@ -37,4 +52,5 @@ class App extends React.Component {
     }
 }
 
-export default App;
+//export default App;
+export default withTranslation('common')(App);
