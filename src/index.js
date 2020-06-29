f44defc5ba85423f88cda3674f3513bde314f878
sigit <sigit.setiaji@vizitrip.com>
2020-06-29 09:31:57 +0700

    first commit react covid19 tracker

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
+import App from "./App";
+import {I18nextProvider} from 'react-i18next';
+import i18next from 'i18next';
+
+import common_id from "./translations/id/common.json";
+import common_en from "./translations/en/common.json";
+
+// i18next.init({
+//     interpolation: { escapeValue: false },  // React already does escaping
+// });
+i18next.init({
+    interpolation: { escapeValue: false },  // React already does escaping
+    lng: 'en',                              // language to use
+    resources: {
+        en: {
+            common: common_en               // 'common' is our custom namespace
+        },
+        id: {
+            common: common_id
+        },
+    },
+});
+
+//ReactDOM.render(<App />, document.getElementById("root"));
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <I18nextProvider i18n={i18next}>
+        <App/>
+    </I18nextProvider>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
